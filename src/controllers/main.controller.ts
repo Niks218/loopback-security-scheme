c2ea41a8613335a6e0b2243d45c541af90ae8c37
Nikita Tsykunov <nikita.tsykunov@notbadmedia.com>
2021-04-21 11:09:38 +0300

    Add security object

--- src/controllers/main.controller.ts
+++ src/controllers/main.controller.ts
@@ -1,14 +1,15 @@
-import { authenticate } from "@loopback/authentication";
+import {authenticate} from "@loopback/authentication";
 import {get, response} from "@loopback/rest";
 import {GenericError} from "../models/error.model";
 
 export class MainController {
-  constructor() {}
-
-  @response(500, GenericError)
-  @authenticate('basic')
-  @get('/test')
-  test() {
-    return;
-  }
+    @response(500, GenericError)
+    @authenticate('basic')
+    @get('/test', {
+        security: [{basic: []}],
+        responses: {}
+    })
+    test() {
+        return;
+    }
 }
